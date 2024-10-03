74bab1d435861036aa2a70247407ec2f78a5992d
Reza Kheradmandi <r.kheradmandi@gmail.com>
2024-10-03 19:38:15 +0330

    the procces of login for generate API is completed

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,8 +3,9 @@ const { check } = require("express-validator");
 
 const router = express.Router();
 
-const { SignUp } = require("../controllers/user-controllers");
+const { SignUp, LogIn } = require("../controllers/user-controllers");
 
-router.post("/signup", [check("email").isEmail(), check("password").isLength({ max: 8 })], SignUp);
+router.post("/signup", [check("email").isEmail(), check("password").isLength({ min: 8 })], SignUp);
+router.post("/login", [check("email").isEmail(), check("password").isLength({ min: 8 })], LogIn);
 
 module.exports = router;
