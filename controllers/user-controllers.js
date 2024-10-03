046dc66c14ddc87ab3bb95adbb9ae6a6f6d44414
Reza Kheradmandi <r.kheradmandi@gmail.com>
2024-10-03 19:38:15 +0330

    the procces of login for generate API is completed

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,5 +1,5 @@
 // prisma client
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient, Prisma } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 // express validator
@@ -29,7 +29,7 @@ async function hashPassword(password) {
 async function comparePasswords(enteredPassword, storedHash) {
     try {
         const match = await bcrypt.compare(enteredPassword, storedHash);
-        return match === undefined ? false : true;
+        return match;
     } catch (error) {
         console.error("Error comparing passwords:", error);
     }
@@ -58,15 +58,24 @@ const SignUp = async (req, res, next) => {
     const token = jwt.sign({ email }, process.env.SECRET_KEY);
     const hashedToken = hashJwtToken(token);
 
-    await prisma.user.create({
-        data: {
-            email: email,
-            password: hashedPassword,
-            auth_token: hashedToken,
-            created_at: Date.now()
+    try {
+        await prisma.user.create({
+            data: {
+                email: email,
+                password: hashedPassword,
+                auth_token: hashedToken,
+                created_at: Date.now()
+            }
+        })
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === 'P2002') {
+                res.status(409).json({
+                    message: "Email already exists!"
+                })
+            }
         }
-    })
-
+    }
 
     res.status(201).json({
         message: "Welocome to Flipit!",
@@ -74,6 +83,53 @@ const SignUp = async (req, res, next) => {
     });
 }
 
+const LogIn = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = errors.array()[0];
+        return res.status(422).json({
+            message: error.msg,
+            field: error.param
+        });
+    }
+
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (user) {
+        const accuratePassword = await comparePasswords(password, user.password);
+        if (accuratePassword) {
+            const token = jwt.sign({ email }, process.env.SECRET_KEY);
+            const hashedToken = hashJwtToken(token);
+            await prisma.user.update({
+                where: {
+                    email
+                },
+                data: {
+                    auth_token: hashedToken
+                }
+            })
+            res.status(200).json({
+                message: "Welcome back!",
+                token
+            })
+        } else {
+            res.status(401).json({
+                message: "Password is incorrect!"
+            })
+        }
+    } else {
+        res.status(404).json({
+            message: "The user does not exist!"
+        })
+    }
+}
+
 module.exports = {
-    SignUp
+    SignUp,
+    LogIn
 }
