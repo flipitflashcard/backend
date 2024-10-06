1ee0409908b95d5be7bd24116df95a5211cd12b5
reza kheradmandi <kheradmandi.partotech@gmail.com>
2024-10-06 16:48:47 +0330

    api boxex created

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+BigInt.prototype.toJSON = function () {
+    return Number(this);
+}
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -9,6 +13,9 @@ app.use(cors());
 
 const user_routes = require("./routes/user-routes");
 app.use("/api/users", user_routes);
+
+const box_routes = require("./routes/box-routes");
+app.use("/api/boxes", box_routes);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
---
+++ controllers/box-controllers.js
@@ -0,0 +1,115 @@
+// prisma client
+const { PrismaClient, Prisma } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const e = require("express");
+
+// Create a new box
+const CreateBox = async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        const { id } = req.payload;
+
+        const box = await prisma.box.create({
+            data: {
+                user_id: id,
+                name,
+                language_code: 'en',
+                created_at: Date.now()
+            },
+        });
+
+        res.status(201).json(box);
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Get all boxes
+const GetBoxes = async (req, res) => {
+    const { id } = req.payload;
+    try {
+        const boxes = await prisma.box.findMany({
+            where: { user_id: parseInt(id) },
+            include: {
+                _count: {
+                    select: {
+                        cards: true
+                    }
+                }
+            }
+        });
+        res.status(200).json(boxes);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// Get a single box by ID
+const GetBox = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { id: userId } = req.payload;
+
+        const box = await prisma.box.findUnique({
+            where: { id: parseInt(id) },
+        });
+
+        if (parseInt(box.user_id) !== userId) {
+            return res.status(403).json({ error: "Unauthorized" });
+        }
+
+        if (!box) {
+            return res.status(404).json({ error: "Box not found" });
+        }
+        res.status(200).json(box);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// Update a box
+const UpdateBox = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        const { id: userId } = req.payload;
+
+        const updatedBox = await prisma.box.update({
+            where: {
+                id: parseInt(id),
+                user_id: parseInt(userId),
+            },
+            data: {
+                name,
+                updated_at: Date.now()
+            },
+        });
+        res.status(200).json(updatedBox);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Delete a box
+const DeleteBox = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await prisma.box.delete({
+            where: { id: parseInt(id) },
+        });
+        res.status(204).send();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    CreateBox,
+    GetBoxes,
+    GetBox,
+    UpdateBox,
+    DeleteBox,
+};
