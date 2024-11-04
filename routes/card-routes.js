e8a92a77d531d01b3e1155b687c1ea3de1673a76
partotech <kheradmandi.partotech@gmail.com>
2024-11-04 12:20:53 +0330

    the srs alghorithm and edit word api is developed

--- routes/card-routes.js
+++ routes/card-routes.js
@@ -6,8 +6,27 @@ const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
-const { CreateWord } = require("../controllers/card-controllers");
+const { CreateWord, ReviewWord, GetDueCards, GetCards, DeleteCard, GetCard, UpdateWord } = require("../controllers/card-controllers");
 
+// Create new card
 router.post("/create", isAuthenticated, CreateWord);
+
+// Review a card
+router.post("/review/:cardId", isAuthenticated, ReviewWord);
+
+// Get due cards for a box
+router.get("/due/:boxId", isAuthenticated, GetDueCards);
+
+// Get all cards for a box
+router.get("/get-cards/:name", isAuthenticated, GetCards);
+
+// delete a cards
+router.delete("/delete-card/:id", isAuthenticated, DeleteCard);
+
+// get one card
+router.get("/get-card/:id", isAuthenticated, GetCard);
+
+// Update existing card
+router.patch("/update/:id", isAuthenticated, UpdateWord);
 
 module.exports = router;
